5f6ebfa78d76d05022e23102cf6a25ca55d875ef
305a385 <132265158+ef3dd1b07c14731a7fe2fd5455e50fc3@users.noreply.github.com>
2023-06-04 07:24:31 +0200

    allowed stopping music by closing wofi

--- .config/bin/music.sh
+++ .config/bin/music.sh
@@ -5,6 +5,11 @@ mpc repeat on
 
 music="$(mpc listall | wofi --dmenu)" # put to variable so current music wont stop
 mpc 'clear' # removes current queue
-mpc add "$music"
+
+if [ "$music" != "" ]; then
+	mpc add "$music"
+else
+	mpc 'clear';
+fi;
 
 mpc play
